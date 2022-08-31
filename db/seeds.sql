6e09999da7359301c129045b4453fef211d591b1
bussudo <105820926+bussudo@users.noreply.github.com>
2022-08-30 21:43:51 -0400

    user database created

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO users (first_name, last_name, github, email, class)
+VALUES ("Shannon", "Kendall", "https://github.com/shannie14", "shannon.marie.kendall@gmail.com", "MSU-VIRT-FSF-PT-05-2022-U-LOLC"),
+       ("DJ", "McMillan", "https://github.com/deejerz88", "djmcmillan93@gmail.com", "MSU-VIRT-FSF-PT-05-2022-U-LOLC"),
+       ("Casey", "Chartier", "https://github.com/MustyBraid", "chartiercasey@gmail.com" "MSU-VIRT-FSF-PT-05-2022-U-LOLC"),
+       ("Donna", "Bussure", "https://github.com/bussudo", "bussudo@gmail.com", "MSU-VIRT-FSF-PT-05-2022-U-LOLC"),
+       ("Jase", "Mucene", "https://github.com/Jmucene", "skysthelimit0594@gmail.com", "MSU-VIRT-FSF-PT-05-2022-U-LOLC")
+ 
