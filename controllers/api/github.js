0e4aa6887526cde6e8275fb6c100d2fe53b40b32
Musty Braid <chartiercasey@gmail.com>
2022-09-01 19:14:18 -0400

    switching over to octokit

--- controllers/api/github.js
+++ controllers/api/github.js
@@ -1,36 +1,15 @@
 const USER = "MustyBraid";
 const EMAIL = "elawsomo@gmail.com";
 const app = require("express").Router();
+
 require("dotenv").config();
 
 //const github = require("octokat")({ token: process.env.GitHub_Key });
 
 //return github
-events = app.get(`https://api.github.com/users/${USER}/events`);
-// .then((events) => {
-//   let lastCommit;
-
-//   events.some((event) => {
-//     return (
-//       event.type === "PushEvent" &&
-//       event.payload.commits.reverse().some((commit) => {
-//         if (commit.author.email === EMAIL) {
-//           lastCommit = {
-//             repo: event.repo.name,
-//             sha: commit.sha,
-//             time: new Date(event.createdAt),
-//             message: commit.message,
-//             url: commit.url,
-//           };
-
-//           return true;
-//         }
-
-//         return false;
-//       })
-//     );
-//   });
-
-//   return lastCommit;
-// });
-console.log(events);
+await octokit.request("GET /repos/{owner}/{repo}/issues", {
+  owner: "octocat",
+  repo: "hello-world",
+  title: "Hello, world!",
+  body: "I created this issue using Octokit!",
+});
