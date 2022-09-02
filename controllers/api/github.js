d948c324490cc82a4d5740d6db67799cd81d0f80
Musty Braid <chartiercasey@gmail.com>
2022-09-01 20:19:26 -0400

    First working API call!

--- controllers/api/github.js
+++ controllers/api/github.js
@@ -1,15 +1,26 @@
-const USER = "MustyBraid";
-const EMAIL = "elawsomo@gmail.com";
 const app = require("express").Router();
+const { Octokit } = require("@octokit/rest");
+//const { Sequelize } = require("sequelize");
+
+const octokit = new Octokit({
+  auth: process.env.GitHub_Key,
+});
 
 require("dotenv").config();
+//our rate limit for these requests with octokit is either 1,000/hour or 5,000/hour, I'm honestly not sure
 
 //const github = require("octokat")({ token: process.env.GitHub_Key });
 
 //return github
-await octokit.request("GET /repos/{owner}/{repo}/issues", {
-  owner: "octocat",
-  repo: "hello-world",
-  title: "Hello, world!",
-  body: "I created this issue using Octokit!",
-});
+
+let username = "MustyBraid"; //this will come from a sequelize query, obviously
+
+async function main() {
+  const events = await octokit.request("GET /users/{username}/events/public", {
+    username: username,
+  });
+  console.log(events);
+  return events;
+}
+
+main();
