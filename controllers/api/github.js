8c7374070855d8aeddc92002694f79ab6dc1ee1c
Musty Braid <chartiercasey@gmail.com>
2022-09-08 18:48:16 -0400

    cleaned github.js and removed code/todo:s that are more appropriate in the front end

--- controllers/api/github.js
+++ controllers/api/github.js
@@ -1,6 +1,4 @@
-const app = require("express").Router();
 const { Octokit } = require("@octokit/rest");
-//const { Sequelize } = require("sequelize");
 
 const octokit = new Octokit({
   auth: process.env.GitHub_Key,
@@ -9,9 +7,6 @@ const octokit = new Octokit({
 require("dotenv").config();
 //our rate limit for these requests with octokit is either 1,000/hour or 5,000/hour, I'm honestly not sure
 //Get a backup API key as well for presentation day
-
-//TO DO: write a function that grabs the github username from a random (or specified, for profile pages) registered user
-let name = "MustyBraid";
 
 function contributionFilter(event) {
   if (
@@ -26,35 +21,31 @@ function contributionFilter(event) {
 }
 
 function uniqueFilter(event) {
-  let urls = []; //this will be an array of unique urls
-  let goodEvents = []; //this will be an array of the event objects containing those unique lists, so we can pull more data from them
+  let urls = [];
+  let goodEvents = []; //This copies the events the urls above are from. They should share the same index number
   for (let i = 0; i < event.length; i++) {
     if (!urls.includes(event[i].repo.url)) {
       urls.push(event[i].repo.url);
       goodEvents.push(event[i]);
     }
-    //find unique urls, append them to the urls array, and append the corresponding objects at index i to the goodEvents array
     //It's just now occurring to me that we can probably just take the first goodEvent for the main page,
     //but this code will be necessary for the profile page implementation
   }
   return [urls, goodEvents];
 }
 
-const request = async () => {
+const singleGithubReq = async (username) => {
   let { data: publicEvents } = await octokit.request(
     "GET /users/{username}/events/public",
     {
-      username: name,
-      per_page: 10,
+      username: username,
+      per_page: 10, //Change this variable to change how many events we're requesting from any one user before filtering
     }
   );
   publicEvents = publicEvents.filter(contributionFilter);
   publicEvents = uniqueFilter(publicEvents);
-  console.log(publicEvents[0]); //This is a little weird, but just use index 0 or 1 to choose what we want to interact with.
+  console.log(publicEvents); //This is a little weird, but just use index 0 or 1 to choose what we want to interact with.
   return publicEvents;
 };
 
-request();
-
-//TO DO: append the results from request to an array, and write those values into the html.
-//This will need logic to make sure we get at least 5 unique repositories, we may need to make calls for more than 5 users to do this
+module.exports(singleGithubReq);
