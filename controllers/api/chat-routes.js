86316cebd602aebea0703ac4b93212f13581beb4
Musty Braid <chartiercasey@gmail.com>
2022-09-08 21:09:32 -0400

    cleaned up a few files in the wake of debugging mysql

--- controllers/api/chat-routes.js
+++ controllers/api/chat-routes.js
@@ -10,7 +10,7 @@ const pusher = new Pusher({
   encrypted: true,
 });
 
-router.get("/:partnerId", async (req, res) => { 
+router.get("/:partnerId", async (req, res) => {
   const partnerId = req.params.partnerId;
   const userId = req.session.userId;
   console.log("partnerId", partnerId, "userId", userId);
@@ -21,23 +21,21 @@ router.get("/:partnerId", async (req, res) => {
     },
   });
   res.json(chat);
-})
+});
 
-
-router.post('/:id', (req, res) => { 
+router.post("/:id", (req, res) => {
   const id = req.params.id;
-  const message = req.body
-  console.log('message', message, 'id', id)
+  const message = req.body;
+  console.log("message", message, "id", id);
 
-  pusher.trigger('We-Boot', `chat-${id}`, message)
-  res.send(message)
-})
+  pusher.trigger("We-Boot", `chat-${id}`, message);
+  res.send(message);
+});
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   const partnerId = req.params.id;
   const userId = req.session.user_id;
   const chat = req.body;
-})
- 
+});
 
 module.exports = router;
