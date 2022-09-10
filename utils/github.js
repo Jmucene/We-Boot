54ba9481602fd5a0cd0ef8dcef639d7e5668de8e
Musty Braid <chartiercasey@gmail.com>
2022-09-10 06:43:05 -0400

    Made it work, more or less. Needs to be polished a great deal

--- utils/github.js
+++ utils/github.js
@@ -1,12 +1,58 @@
 const { Octokit } = require("@octokit/rest");
+require("dotenv").config();
 
 const octokit = new Octokit({
   auth: process.env.GitHub_Key,
+  //auth: process.env.Backup_GitHub_Key,
 });
 
-require("dotenv").config();
 //our rate limit for these requests with octokit is either 1,000/hour or 5,000/hour, I'm honestly not sure
 //Get a backup API key as well for presentation day
+
+let hardcodedCringe = [
+  { name: "Casey Chartier", username: "MustyBraid" },
+  { name: "Milantea Adams", username: "milantea" },
+  { name: "Shannon Kendall", username: "shannie14" },
+  { name: "DJ McMillan", username: "deejerz88" },
+  { name: "Donna Bussure", username: "bussudo" },
+  { name: "Jase Mucene", username: "Jmucene" },
+  { name: "Mim Armand", username: "mim-Armand" },
+  { name: "Austin Leblanc", username: "austinleblanc" },
+  { name: "Derek Hoye", username: "DLHoye" },
+  { name: "Justin Thon", username: "Limpbrick" },
+  { name: "Doug Schulte", username: "dkschulte" },
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function homepageList(length) {
+  let final = [];
+  let chosenUsers = [];
+  for (let i = 0; i < length; ) {
+    let randomUser = hardcodedCringe[getRandomInt(hardcodedCringe.length)];
+
+    if (!chosenUsers.includes(randomUser.username)) {
+      chosenUsers.push(randomUser.username);
+      a = await githubRequest(randomUser.username, 5);
+      if (a.urls) {
+        for (let j = 0; j < a.urls.length; ) {
+          if (!final.includes(a.urls[j])) {
+            final.push(
+              `${randomUser.name} recently updated a project at ${a.urls[j]}`
+            );
+            i++;
+            break;
+          } else {
+            j++;
+          }
+        }
+      }
+    }
+  }
+  return final;
+}
 
 function contributionFilter(event) {
   if (
@@ -44,8 +90,10 @@ const githubRequest = async (username, number) => {
   );
   publicEvents = publicEvents.filter(contributionFilter);
   publicEvents = uniqueFilter(publicEvents);
-  console.log(publicEvents); //note that this is a length 2 array defined in uniqueFilter
-  return publicEvents;
+  formattedEvents = { urls: publicEvents[0], repos: publicEvents[1] };
+  //console.log(publicEvents); //note that this is a length 2 array defined in uniqueFilter
+  return formattedEvents;
 };
 
-module.exports = githubRequest;
+homepageList(5);
+module.exports = { githubRequest, homepageList };
