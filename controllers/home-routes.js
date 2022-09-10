7e60d450c006c1b48b97f408b3895b581714c15c
bussudo <105820926+bussudo@users.noreply.github.com>
2022-09-10 10:50:55 -0400

    added search functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const { User } = require('../models');
+const router = require("express").Router();
+const { User } = require("../models");
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
@@ -10,12 +10,12 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll({
       raw: true,
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
     });
 
     console.log("userData", userData);
@@ -36,8 +36,43 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get("/search", async (req, res) => {
+  res.render("search");
+});
+router.get("/searchName/:name", (req, res) => {
+  // Get one alum from the alum table
 
+  User.findOne({
+    // Gets the alum based on the name given in the request parameters
+    where: {
+      name: req.params.name,
+    },
+  })
+    .then((nameData) => {
+      res.json(nameData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.get("/searchGithub/:github", (req, res) => {
+  // Get one alum from the alum table
 
+  User.findOne({
+    // Gets the alum based on the github given in the request parameters
+    where: {
+      github: req.params.github,
+    },
+  })
+    .then((githubData) => {
+      res.json(githubData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
